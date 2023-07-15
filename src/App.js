981b45104e6a4e337bf98d948297c1cecb96fa3a
Wenyu Zheng <zhengwy229@gmail.com>
2023-07-15 18:51:49 +0100

    User story 4

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <div id="session-label">Session Length</div>
       <button id="break-decrement">Break-</button>
       <button id="session-decrement">Session-</button>
+      <button id="break-increment">Break+</button>
+      <button id="session-increment">Session+</button>
     </div>
   );
 }
